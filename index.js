b311c79fccec8070fdc15064dc1aff9d08f3678b
Ibrahim Hadi Abdali <ibrahimha@outlook.com>
2021-09-23 16:01:12 +0300

    commit interface

--- index.js
+++ index.js
@@ -66,24 +66,24 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const bot = new Composer();
 
-interface IMessage {
-  message_id: number;
-  from: {
-    id: number,
-    is_bot: boolean,
-    first_name: string,
-    last_name: string,
-    language_code: "ar" | "en",
-  };
-  chat: {
-    id: number,
-    first_name: string,
-    last_name: string,
-    type: string,
-  };
-  date: number;
-  text: string; // The actual message
-}
+// interface IMessage {
+//   message_id: number;
+//   from: {
+//     id: number,
+//     is_bot: boolean,
+//     first_name: string,
+//     last_name: string,
+//     language_code: "ar" | "en",
+//   };
+//   chat: {
+//     id: number,
+//     first_name: string,
+//     last_name: string,
+//     type: string,
+//   };
+//   date: number;
+//   text: string; // The actual message
+// }
 bot.start((ctx) => ctx.reply("ارحب"));
 
 bot.on("text", (ctx) => {
