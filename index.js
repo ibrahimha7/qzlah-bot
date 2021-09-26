fe3d9896aa51aead78609902eb4a10e951342831
Ibrahim Hadi Abdali <ibrahimha@outlook.com>
2021-09-26 20:42:51 +0300

    adding calires

--- index.js
+++ index.js
@@ -5,25 +5,27 @@ const axios = require("axios");
 const bot = new Composer();
 const bot_api = process.env.BOT_API_SERVER;
 
-bot.start((ctx) => ctx.reply("ارحب"));
-
-bot.on("text", (ctx) => {
-  let message = ctx.message.text;
-
-  ctx.reply(`Your calories: ${getFood(message)}`);
-  // ctx.reply(`باقي شين؟`);
-});
-
-const getFood = (food) => {
-  getFoodFromApi(food).then((res) => {
+let foodCalories;
+const getFood = async (food) => {
+  await getFoodFromApi(food).then((res) => {
+    foodCalories = res.cal;
     return res.cal;
   });
 };
-const getFoodFromApi = (food) => {
-  return axios.get(`${bot_api}cal/${food}`).then((response) => {
+const getFoodFromApi = async (food) => {
+  return await axios.get(`${bot_api}cal/${food}`).then((response) => {
     response = response.data;
     return response;
   });
 };
+
+bot.start((ctx) => ctx.reply("ارحب"));
+
+bot.on("text", (ctx) => {
+  let message = ctx.message.text;
+  getFood(message);
+  ctx.reply(`سعراتك: ${foodCalories} يازلمه`);
+  ctx.reply(`باقي شين؟`);
+});
 
 module.exports = bot;
